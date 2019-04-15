8f9d921083ca064c87fd751f54d1706aaf6b00b5
Jose Trinidad <internick18@gmail.com>
2019-04-15 18:52:46 -0500

    Initial Commit

---
+++ pixel-city/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pixel-city
+//
+//  Created by Admin on 15/04/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
